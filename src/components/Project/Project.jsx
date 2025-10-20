24306965f8a415d55daafbe548dfa6cd87d1c69a
Dhanush M <mdhanushraju345@gmail.com>
2025-10-20 22:44:23 +0530

    project com

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,84 +1,61 @@
 import React from 'react'
 import "./Project.css"
-import recipeImg from '../assets/recipe.png';
+import recipeImg from '../assets/recipe.png'
+import Portfolio from '../assets/portfolio.png'
 
 export default function Project() {
   return (
     <>
-    <h1 className='text-center'>Projects:-</h1>
-    <div className="container projects" id='project'>
-      
-    <div className='po'>   
-      <div className="container p-1">
-        
+      <h1 className='text-center'>Projects:-</h1>
+      <div className="projects" id='project'>
+        <div className="po">
+          <div className="po-container">
             <div className="card-1">
-              <div className="card-title bg-dark-subtle text-center"><h3>Recipe Finder</h3></div>
+              <div className="card-title bg-dark-subtle text-center">
+                <h3>Recipe Finder</h3>
+              </div>
               <div className="card-body body-1">
                 <img src={recipeImg} alt="" className='img' />
               </div>
               <div className="card-footer foot-1 ">
-                   <button className='btn btn-success '><a href=" https://mdhanushraju.github.io/Project-Food-recipe/" target='blank' className='a-1 ' style={{textDecoration:"none",color:"white"}}>view</a></button>
+                <button className='btn btn-success'>
+                  <a href="https://mdhanushraju.github.io/Project-Food-recipe/" target='_blank' className='a-1' style={{textDecoration:"none",color:"white"}}>view</a>
+                </button>
               </div>
+            </div>
+          </div>
+         <div className="po-container">
+            <div className="card-1">
+              <div className="card-title bg-dark-subtle text-center">
+                <h3>Protfolio</h3>
+              </div>
+              <div className="card-body body-1">
+                <img src={Portfolio} alt="" className='img' />
+              </div>
+              <div className="card-footer foot-1 ">
+                <button className='btn btn-success'>
+                  <a href="" target='_blank' className='a-1' style={{textDecoration:"none",color:"white"}}>view</a>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="po-container">
+            <div className="card-1">
+              <div className="card-title bg-dark-subtle text-center">
+                <h3>Empty</h3>
+              </div>
+              <div className="card-body body-1">
+                <img src="" alt="" className='img' />
+              </div>
+              <div className="card-footer foot-1 ">
+                <button className='btn btn-success'>
+                  <a href="" target='_blank' className='a-1' style={{textDecoration:"none",color:"white"}}>view</a>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      {/* <div className="container p-2">
-         <div className="card-2">
-              <div className="card-body body-2">
-                <img src="" alt="" className='img' />
-              </div>
-              <div className="card-footer foot-2 ">
-                   <button className='btn btn-danger'>about</button>
-                   <button className='btn btn-success'>view</button>
-              </div>
-        </div>
-        </div>
-      <div className="container p-3"> 
-        <div className="card-3">
-              <div className="card-body body-3">
-                <img src="" alt="" className='img' />
-              </div>
-              <div className="card-footer foot-3 ">
-                   <button className='btn btn-danger'>about</button>
-                   <button className='btn btn-success'>view</button>
-              </div>
-        </div>
-        </div>
-       <div className="container p-4"> 
-  <div className="card-4">
-    <div className="card-body body-4">
-      <img src="" alt="" className='img' />
-    </div>
-    <div className="card-footer foot-4">
-      <button className='btn btn-danger'>about</button>
-      <button className='btn btn-success'>view</button>
-    </div>
-  </div>
-</div>
-<div className="container p-5"> 
-  <div className="card-5">
-    <div className="card-body body-5">
-      <img src="" alt="" className='img' />
-    </div>
-    <div className="card-footer foot-5">
-      <button className='btn btn-danger'>about</button>
-      <button className='btn btn-success'>view</button>
-    </div>
-  </div>
-</div>
-
-       <div className="container p-6"> 
-        <div className="card-6">
-              <div className="card-body body-6">
-                <img src="" alt="" className='img' />
-              </div>
-              <div className="card-footer foot-6 ">
-                   <button className='btn btn-danger'>about</button>
-                   <button className='btn btn-success'>view</button>
-              </div>
-        </div>
-        </div> */}
-        </div>
-    </div>
     </>
   )
 }
